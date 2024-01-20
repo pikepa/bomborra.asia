04ce63a88016ce6661730888ce5d1f31a3f0063a
Peter Pike <pikepeter@gmail.com>
2024-01-20 10:21:58 +1000

    modifications to the installation of fontawesome, remove cdn entries from gueat and app layouts, modified vite config to allow connection to secure server

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,46 @@
-import laravel from 'laravel-vite-plugin'
-import {defineConfig} from 'vite'
+import laravel from 'laravel-vite-plugin';
+import {defineConfig} from 'vite';
+import fs from 'fs';
+const host = 'bomborra.test';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    });
-                }
-            },
-        }
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            detectTls: host,
+            refresh: true,
+        }),
     ],
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: fs.readFileSync(`/Users/peterpike/Library/Application Support/Herd/config/valet/Certificates/${host}.key`),
+            cert: fs.readFileSync(`/Users/peterpike/Library/Application Support/Herd/config/valet/Certificates/${host}.crt`),
+        }
+    }
 });
+// export default defineConfig({
+//     plugins: [
+//         laravel([
+//             'resources/css/app.css',
+//             'resources/js/app.js',
+//         ]),
+//         {
+//             name: 'blade',
+//             handleHotUpdate({ file, server }) {
+//                 if (file.endsWith('.blade.php')) {
+//                     server.ws.send({
+//                         type: 'full-reload',
+//                         path: '*',
+//                     });
+//                 }
+//             },
+//         }
+//     ],
+// });
+
+
