1a44f96a853878afef8f492fe7647df282962515
Peter Pike <pikepeter@gmail.com>
2024-01-23 16:28:37 +1000

    modified name to advanced Filter

--- resources/views/livewire/posts/manage-posts.blade.php
+++ resources/views/livewire/posts/manage-posts.blade.php
@@ -34,7 +34,7 @@
         </div>
         <div class="ml-4">
           <x-button.link wire:click="$toggle('showFilters')">
-            @if ($showFilters) Hide @endif Advanced Search...</Search></x-button.link>
+            @if ($showFilters) Hide @endif Advanced Filters...</Search></x-button.link>
         </div>
       </div>
       <!-- Advanced Search -->
