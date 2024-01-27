c716147449ddbd5281e62b6b7d3d6dfbe20d5aa3
Peter Pike <pikepeter@gmail.com>
2024-01-27 16:57:10 +1000

    Added link to open post

--- resources/views/livewire/home/main-body.blade.php
+++ resources/views/livewire/home/main-body.blade.php
@@ -1,23 +1,28 @@
 <div class="bg-cyan-100 p-2">
+
+
     <!-- source https://www.epicweb.dev/tutorials/fluid-hover-cards-with-tailwind-css/implementation/concluding-the-fluid-hover-cards-tutorial -->
     <div class="grid  place-items-center ">
         <ul class="flex gap-4 w-full ">
             @foreach($posts as $post)
             <li
                 class="flex-1 hover:grow-[1.3] transition-all bg-rose-300 h-[400px] w-full rounded-2xl relative overflow-hidden group">
-                <img class="absolute h-full w-full inset-0 object-cover" src='{{$post->cover_image}}' alt="placeimg">
-                <div class="absolute inset-x-0 bottom-0 p-4 bg-gradient-to-t from-black from-30% ">
-                    <div class="w-64">
-                        <h2 class="text-2xl text-white font-medium leading-tight">{{$post->title}}</h2>
-                        <div class="grid grid-rows-[0fr] group-hover:grid-rows-[1fr] transition-all">
-                            <p class="mt-2 overflow-hidden text-white/70 
+                <a href="/posts/{{$post->slug}}">
+                    <img class="absolute h-full w-full inset-0 object-cover" src='{{$post->cover_image}}'
+                        alt="placeimg">
+                    <div class="absolute inset-x-0 bottom-0 p-4 bg-gradient-to-t from-black from-30% ">
+                        <div class="w-64">
+                            <h2 class="text-2xl text-white font-medium leading-tight">{{$post->title}}</h2>
+                            <div class="grid grid-rows-[0fr] group-hover:grid-rows-[1fr] transition-all">
+                                <p class="mt-2 overflow-hidden text-white/70 
                                  opacity-0 group-hover:opacity-100 duration-300 transition text-clip ">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, pariatur.
-                            </p>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, pariatur.
+                                </p>
+                            </div>
                         </div>
-                    </div>
 
-                </div>
+                    </div>
+                </a>
             </li>
             @if($loop->iteration == 4)
             @break
@@ -41,4 +46,5 @@
         </div>
         @endif
     </div>
+
 </div>
