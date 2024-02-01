e67f502e524080f26d9be6c7fa9e431ff838b1d1
Peter Pike <pikepeter@gmail.com>
2024-02-02 09:30:19 +1000

    Sitemap code added and sitemap generated

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Post;
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+
+class GenerateSitemap extends Command
+{
+    protected $signature = 'sitemap:generate';
+
+    protected $description = 'Generate sitemap';
+
+    public function handle()
+    {
+        // Manually create sitemap
+        $sitemap = Sitemap::create();
+
+        // Static pages
+        $sitemap->add('/');
+
+        // Dynamic pages
+        $posts = Post::all();
+        foreach ($posts as $post) {
+            $sitemap->add("/posts/{$post->slug}");
+        }
+
+        $sitemap->writeToFile(public_path('sitemap_posts.xml'));
+    }
+}
