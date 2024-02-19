bfb35e70967d2088cf86e82f78355e1efaa7a07a
Peter Pike <pikepeter@gmail.com>
2024-02-20 07:26:19 +1000

    removed dashboard from header menu

--- resources/views/components/dash-header.blade.php
+++ resources/views/components/dash-header.blade.php
@@ -1,7 +1,7 @@
 <div class="mb-2 min-h-34 bg-slate-200 rounded-lg flex flex-row justify-between">
     <div class="-ml-1 flex flex-row text-xl font-semibold space-x-4 items-center ">
         <div class="inline p-2 cursor-pointer"><a href="{{ route('home') }}">Home</a></div>
-        <div class="inline p-2 cursor-pointer" ><a href="{{ url('dashboard/dash') }}">Dashboard</a></div>
+        <!-- <div class="inline p-2 cursor-pointer" ><a href="{{ url('dashboard/dash') }}">Dashboard</a></div> -->
         <div class="inline p-2 cursor-pointer" ><a href="{{ url('dashboard/categories') }}">Categories</a></div>
         <div class="inline p-2 cursor-pointer" ><a href="{{ url('dashboard/channels') }}">Channels</a></div>
         <div class="inline p-2 cursor-pointer" ><a href="{{ url('dashboard/posts') }}">Posts</a></div>
