bcb3da2594a9905f4cfc99b74888598009f6ad80
Peter Pike <pikepeter@gmail.com>
2024-07-28 16:51:19 +0530

    various miscellaneous updates to app service providor and edit post

--- database/migrations/2023_02_26_153008_add_channel_id_to_posts_table.php
+++ database/migrations/2023_02_26_153008_add_channel_id_to_posts_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -26,7 +26,6 @@ return new class() extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
-        });
+        Schema::table('posts', function (Blueprint $table) {});
     }
 };
